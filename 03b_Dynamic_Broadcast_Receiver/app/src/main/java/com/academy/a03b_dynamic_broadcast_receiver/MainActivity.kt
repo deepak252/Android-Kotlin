79620227700336b50a2c536db2f1731be523a29f
Deepak <trojanhorsedc@gmail.com>
2022-12-21 12:18:55 +0530

    Dynamic Broadcast: Starting files

---
+++ 03b_Dynamic_Broadcast_Receiver/app/src/main/java/com/academy/a03b_dynamic_broadcast_receiver/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.academy.a03b_dynamic_broadcast_receiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

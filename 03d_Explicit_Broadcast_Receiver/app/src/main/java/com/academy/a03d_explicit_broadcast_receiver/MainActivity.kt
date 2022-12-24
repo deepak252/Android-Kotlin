f0117f1bed49313ec3a3bc249bd45a7ed8e30cd8
Deepak <trojanhorsedc@gmail.com>
2022-12-24 12:14:25 +0530

    Explicit Broadcast : Starting files

---
+++ 03d_Explicit_Broadcast_Receiver/app/src/main/java/com/academy/a03d_explicit_broadcast_receiver/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.academy.a03d_explicit_broadcast_receiver
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
---
+++ 03d_Explicit_Broadcast_Sender/app/src/main/java/com/academy/a03d_explicit_broadcast_sender/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.academy.a03d_explicit_broadcast_sender
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

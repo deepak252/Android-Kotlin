b0837b5a04f17eac12ee877d0d612815f27ec4f0
Deepak <trojanhorsedc@gmail.com>
2022-12-25 01:19:36 +0530

    Threads Mange UI : Starting files

---
+++ 05a_Threads_Manage_UI/app/src/main/java/com/academy/a05a_threads_manage_ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.academy.a05a_threads_manage_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

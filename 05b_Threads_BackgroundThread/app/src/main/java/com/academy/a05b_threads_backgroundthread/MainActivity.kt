6f901143dca1f67a79c5a82ea131a5a6ba8f242b
Deepak <trojanhorsedc@gmail.com>
2022-12-26 01:01:23 +0530

    Background Thread : Send message from BG Thread to Main(UI) Thread

--- 05b_Threads_BackgroundThread/app/src/main/java/com/academy/a05b_threads_backgroundthread/MainActivity.kt
+++ 05b_Threads_BackgroundThread/app/src/main/java/com/academy/a05b_threads_backgroundthread/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.Message
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -15,11 +16,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnRun : Button
     private lateinit var btnClear : Button
     private lateinit var progressIndicator : ProgressBar
+    private lateinit var handler: Handler
+    private var MESSAGE_KEY = "message_key"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
+        // Looper loops on Work Queue
+        // Handler manages the Looper
+        //** This "handler" is attached on Main Thread, so passing Main Thread Looper (getMainLooper)
+        handler =object : Handler(Looper.getMainLooper()){
+            override fun handleMessage(msg: Message) {
+//                super.handleMessage(msg)
+                Log.d("handleMessage", "${msg.data.getString(MESSAGE_KEY)}")
+                // Message Received from Background Thread -> Update UI
+                logMessage("\n${msg.data.getString(MESSAGE_KEY)}")
+                showProgressIndicator(false)
+            }
+        }
+
+    }
+
+    private fun initViews(){
         edtCode = findViewById(R.id.edtCode)
         btnRun = findViewById(R.id.btnRun)
         btnClear = findViewById(R.id.btnClear)
@@ -35,14 +55,13 @@ class MainActivity : AppCompatActivity() {
         btnRun.setOnClickListener{
             runCode()
         }
-
     }
 
     private fun runCode(){
         logMessage("\nRunning Code")
         showProgressIndicator(true)
 //        Thread.sleep(4000)  // Blocks UI thread (Main Thread)
-
+       /* METHOD 1 */
 //        val thread =object : Thread(){
 //            override fun run() {
 //                super.run()
@@ -54,11 +73,32 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        thread.start()
 
-        val runnable =object : Runnable{
+        /* METHOD 2 */
+//        val runnable =object : Runnable{
+//            override fun run() {
+//                Log.d("Background Thread", "Started")
+//                Thread.sleep(4000)
+//                Log.d("Background Thread", "Completed")
+//            }
+//        }
+//        val thread = Thread(runnable)
+//        thread.start()
+
+        /* METHOD 3 */
+        // Implementing Background Thread
+        var runnable =object:  Runnable{
             override fun run() {
-                Log.d("Background Thread", "Started")
+//                logMessage("\nBG Thread : Running Code")
+                Log.d("Background Thread", "Download Started")
                 Thread.sleep(4000)
-                Log.d("Background Thread", "Completed")
+                Log.d("Background Thread", "Download Completed")
+
+                val message = Message()
+                val bundle = Bundle()
+                bundle.putString(MESSAGE_KEY,"Download Completed")
+                message.data = bundle
+                handler.sendMessage(message)  // Send message from background thread (this) to UI thread
+
             }
         }
         val thread = Thread(runnable)

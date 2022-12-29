16939b6854b079b2006c191e5772488d4579952a
Deepak <trojanhorsedc@gmail.com>
2022-12-29 16:53:35 +0530

    Threads Executer Service : Starting files

---
+++ 05f_Threads_Executor_Service/app/src/main/java/com/academy/a05f_threads_executor_service/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.academy.a05f_threads_executor_service
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var edtCode : EditText
+    private lateinit var btnRun : Button
+    private lateinit var btnClear : Button
+    private lateinit var progressIndicator : ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+
+    }
+
+    private fun initViews(){
+        edtCode = findViewById(R.id.edtCode)
+        btnRun = findViewById(R.id.btnRun)
+        btnClear = findViewById(R.id.btnClear)
+        progressIndicator = findViewById(R.id.progressIndicator)
+        showProgressIndicator(false)
+
+        edtCode.setText(R.string.lorem_ipsum)
+
+        btnClear.setOnClickListener{
+            clearCode()
+        }
+
+        btnRun.setOnClickListener{
+            runCode()
+        }
+    }
+
+    private fun runCode(){
+        logMessage("\nRunning Code")
+        showProgressIndicator(true)
+
+    }
+
+
+    fun logMessage(message:String){
+        edtCode.append(message)
+        scrollToEnd()
+    }
+
+    private fun scrollToEnd(){
+        edtCode.scrollTo(0,edtCode.bottom)
+    }
+
+    private fun clearCode(){
+        edtCode.setText("")
+    }
+
+    private fun showProgressIndicator(loading:Boolean){
+        progressIndicator.visibility= if(loading)  View.VISIBLE else View.INVISIBLE
+    }
+
+}

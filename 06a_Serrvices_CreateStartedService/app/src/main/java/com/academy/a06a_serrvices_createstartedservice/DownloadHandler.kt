da06c4dfcbdd193afe200fd655da69f67e5f3be2
Deepak <trojanhorsedc@gmail.com>
2023-01-05 02:15:07 +0530

    Services-Started Service : Back to Thread, Handler, Looper

--- 06a_Serrvices_CreateStartedService/app/src/main/java/com/academy/a06a_serrvices_createstartedservice/DownloadHandler.kt
+++ 06a_Serrvices_CreateStartedService/app/src/main/java/com/academy/a06a_serrvices_createstartedservice/DownloadHandler.kt
@@ -1,20 +1,20 @@
-//package com.academy.a06a_serrvices_createstartedservice
-//
-//import android.os.Handler
-//import android.os.Looper
-//import android.os.Message
-//import android.util.Log
-//
-//
-//class DownloadHandler(looper : Looper) : Handler(looper) {
-//    override fun handleMessage(msg: Message) {
-//        downloadSong(msg.obj.toString())
-//        super.handleMessage(msg)
-//    }
-//
-//    private fun downloadSong( song : String){
-//        Log.d("DownloadHandler", "Starting Download - $song")
-//        Thread.sleep(4000)
-//        Log.d("DownloadHandler", "Download Complete - $song")
-//    }
-//}
+package com.academy.a06a_serrvices_createstartedservice
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+
+
+class DownloadHandler(looper : Looper) : Handler(looper) {
+    override fun handleMessage(msg: Message) {
+        downloadSong(msg.obj.toString())
+        super.handleMessage(msg)
+    }
+
+    private fun downloadSong( song : String){
+        Log.d("DownloadHandler", "Starting Download - $song")
+        Thread.sleep(4000)
+        Log.d("DownloadHandler", "Download Complete - $song")
+    }
+}
--- 06a_Serrvices_CreateStartedService/app/src/main/java/com/academy/a06a_serrvices_createstartedservice/MyDownloadService.kt
+++ 06a_Serrvices_CreateStartedService/app/src/main/java/com/academy/a06a_serrvices_createstartedservice/MyDownloadService.kt
@@ -9,21 +9,31 @@ import android.util.Log
 import com.academy.a05c_thread_backgroundthread_class.Playlist
 
 class MyDownloadService : Service() {
+    var mDownloadThread : DownloadThread?=null
 
     override fun onCreate() {
         super.onCreate()
         Log.d("MyDownloadService","onCreate Called")
+        mDownloadThread = DownloadThread()
+        mDownloadThread?.start()
+        while(mDownloadThread?.mHandler==null){
+            Log.d("MyDownloadService","onCreate : mHandler is null")
+        }
     }
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val song =intent?.getStringExtra(MainActivity.MESSAGE_KEY)
-        Log.d("MyDownloadService","onStartCommand Called")
+        Log.d("MyDownloadService","onStartCommand Called, startId = $startId")
 
-        val myDownloadTask = MyDownloadTask()
         if(song!=null) {
-            // separate AsyncTask execution for each song
-            myDownloadTask.execute(song)
+            if(mDownloadThread?.mHandler!=null){
+                val message = Message()
+                message.obj = song
+                mDownloadThread!!.mHandler!!.sendMessage(message)
+            }else{
+                Log.d("onStartCommand","mHandler is NULL")
+            }
         }
 //        return super.onStartCommand(intent, flags, startId)
         return Service.START_REDELIVER_INTENT
@@ -39,22 +49,22 @@ class MyDownloadService : Service() {
         super.onDestroy()
     }
 
-    class MyDownloadTask : AsyncTask<String, String,String>(){
-        override fun doInBackground(vararg songs: String?): String {
-            for(song in songs){
-                Log.d("MyDownloadTask","onProgressUpdate, Downloading song - $song")
-                Thread.sleep(4000)
-
-            }
-            return "All songs have been downloaded"
-        }
-
-        override fun onProgressUpdate(vararg values: String?) {
-            Log.d("MyDownloadTask","onProgressUpdate, Song Downloaded - ${values[0]}")
-        }
-        override fun onPostExecute(result: String?) {
-            Log.d("MyDownloadTask","onPostExecute, $result")
-        }
-    }
+//    class MyDownloadTask : AsyncTask<String, String,String>(){
+//        override fun doInBackground(vararg songs: String?): String {
+//            for(song in songs){
+//                Log.d("MyDownloadTask","onProgressUpdate, Downloading song - $song")
+//                Thread.sleep(4000)
+//
+//            }
+//            return "All songs have been downloaded"
+//        }
+//
+//        override fun onProgressUpdate(vararg values: String?) {
+//            Log.d("MyDownloadTask","onProgressUpdate, Song Downloaded - ${values[0]}")
+//        }
+//        override fun onPostExecute(result: String?) {
+//            Log.d("MyDownloadTask","onPostExecute, $result")
+//        }
+//    }
 
 }
